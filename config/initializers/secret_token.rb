6bcf947b012526ea4ace38877d015f9b069106c5
seyongha <seyongha@facebook.com>
2011-09-02 02:36:22 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnsApp::Application.config.secret_token = 'bdd7f4d36a817410d391ea87aa706d9abecc17c7b001613e75dfce0595c53563622f089b00ae78cb1c5ad157e6c9e52af3eae784d2f298de25a2cffe11511127'
